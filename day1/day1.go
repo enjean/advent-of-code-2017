2e96b2ad62a0ea10363df64e5d8438912c005084
Eric Jean <eric.jean@monsanto.com>
2019-06-07 09:54:22 -0600

    Day 1 Part 2

--- day1/day1.go
+++ day1/day1.go
@@ -23,10 +23,29 @@ func Captcha(input string) int {
 	return sum
 }
 
+func Captcha2(input string) int {
+	sum := 0
+
+	half := len(input) / 2
+	for i := 0; i < len(input); i++ {
+		j := (i + half) % len(input)
+		if input[i] == input[j] {
+			val := int(input[i]) - '0'
+			sum += val
+		}
+	}
+
+
+	return sum
+}
+
 func main() {
 	bytes, _ := ioutil.ReadFile("day1/input.txt")
 	input := string(bytes)
 
 	captcha := Captcha(input)
 	fmt.Printf("Part 1: %d\n", captcha)
+
+	captcha2 := Captcha2(input)
+	fmt.Printf("Part 2: %d\n", captcha2)
 }
--- day1/day1_test.go
+++ day1/day1_test.go
@@ -20,3 +20,28 @@ func TestCaptcha(t *testing.T) {
 		}
 	}
 }
+
+func TestCaptcha2(t *testing.T) {
+	//1212 produces 6: the list contains 4 items, and all four digits match the digit 2 items ahead.
+	//1221 produces 0, because every comparison is between a 1 and a 2.
+	//123425 produces 4, because both 2s match each other, but no other digit has a match.
+	//123123 produces 12.
+	//12131415 produces 4.
+	tests := []struct {
+		input string
+		expected int
+	}{
+		{"1212", 6},
+		{"1221", 0},
+		{"123425", 4},
+		{"123123", 12},
+		{"12131415", 4},
+	}
+
+	for _, test := range tests {
+		result := Captcha2(test.input)
+		if result != test.expected {
+			t.Errorf("Captcha(%s) expected %d, got %d", test.input, test.expected, result)
+		}
+	}
+}
