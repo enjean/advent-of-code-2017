dde1dac105ac959ce3275a205bc7e0d68b8940bb
Eric Jean <eric.jean@monsanto.com>
2019-06-10 13:39:06 -0600

    Day 5 Part 2

--- day5/day5.go
+++ day5/day5.go
@@ -42,6 +42,25 @@ func Execute(p program) int {
 	return steps
 }
 
+func Execute2(p program) int {
+	steps := 0
+	ip := 0
+	for ip >= 0 && ip < len(p) {
+		//fmt.Printf("%s\n", p.Print(ip))
+		steps++
+		jump := p[ip]
+		if p[ip] >= 3 {
+			p[ip]--
+		} else {
+			p[ip]++
+		}
+		ip += jump
+	}
+
+	//fmt.Printf("%s\n", p.Print(ip))
+	return steps
+}
+
 func main() {
 	file, err := os.Open("day5/input.txt")
 	if err != nil {
@@ -51,12 +70,19 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
-	var program []int
+	var p []int
 	for scanner.Scan() {
 		lineAsInt, _ := strconv.Atoi(scanner.Text())
-		program = append(program, lineAsInt)
+		p = append(p, lineAsInt)
 	}
 
-	steps := Execute(program)
+	program1 := make(program, len(p))
+	copy(program1, p)
+	steps := Execute(program1)
 	fmt.Printf("Part 1: %d\n", steps)
+
+	program2 := make(program, len(p))
+	copy(program2, p)
+	steps2 := Execute2(program2)
+	fmt.Printf("Part 2: %d\n", steps2)
 }
--- day5/day5_test.go
+++ day5/day5_test.go
@@ -17,3 +17,19 @@ func TestExecute(t *testing.T) {
 		t.Errorf("Expected 5 to Execute, was %d", steps)
 	}
 }
+
+func TestExecute2(t *testing.T) {
+	program := []int{
+		0,
+		3,
+		0,
+		1,
+		-3,
+	}
+
+	steps := Execute2(program)
+
+	if steps != 10 {
+		t.Errorf("Expected 10 to Execute2, was %d", steps)
+	}
+}
