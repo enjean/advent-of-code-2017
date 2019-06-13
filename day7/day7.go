29f9b21592003a0c7f605799e89864ac4001048a
Eric Jean <eric.jean@monsanto.com>
2019-06-13 12:55:37 -0600

    Day 7 Part 2

--- day7/day7.go
+++ day7/day7.go
@@ -6,19 +6,15 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
 type programSpec struct {
 	name     string
+	weight   int
 	children []string
 }
-
-//type program struct {
-//	name     string
-//	parent   *program
-//	children []*program
-//}
 
 func FindRoot(input []programSpec) string {
 	appearsAsChild := make(map[string]bool)
@@ -35,7 +31,88 @@ func FindRoot(input []programSpec) string {
 	panic("could not find")
 }
 
-var lineRexexp = regexp.MustCompile(`([a-z]+) \([\d]+\)(?: -> (.*))?`)
+type program struct {
+	name           string
+	weight         int
+	towerWeight    int
+	parent         *program
+	children       []*program
+}
+
+func (p *program) isBalanced() bool {
+	if p.children == nil {
+		return true
+	}
+	childWeightCounts := make(map[int]int)
+	for _, child := range p.children {
+		childWeightCounts[child.towerWeight]++
+	}
+	return len(childWeightCounts) == 1
+}
+
+func FindProgramToAdjust(programs []programSpec) (string, int) {
+	programsByName := make(map[string]programSpec)
+	for _, program := range programs {
+		programsByName[program.name] = program
+	}
+
+	root := FindRoot(programs)
+
+	rootNode := buildNode(programsByName, root)
+
+	nodesToProcess := []*program {rootNode}
+
+	var targetNode *program
+	for {
+		n := len(nodesToProcess) - 1
+		node := nodesToProcess[n]
+		nodesToProcess = nodesToProcess[:n]
+
+		childWeightCounts := make(map[int]int)
+		for _, child := range node.children {
+			childWeightCounts[child.towerWeight]++
+		}
+
+		if len(childWeightCounts) == 1 {
+			targetNode = node
+			break
+		}
+		for _, child := range node.children {
+			if childWeightCounts[child.towerWeight] == 1 {
+				nodesToProcess = append(nodesToProcess, child)
+			}
+		}
+	}
+
+	parentOfTarget := targetNode.parent
+	var targetTowerWeight int
+	for _, child := range parentOfTarget.children {
+		if child.towerWeight != targetNode.towerWeight {
+			targetTowerWeight = child.towerWeight
+			break
+		}
+	}
+	weightDiff := targetTowerWeight - targetNode.towerWeight
+
+	return targetNode.name, targetNode.weight + weightDiff
+}
+
+func buildNode(programsByName map[string]programSpec, currentNode string) *program {
+	p := &program{
+		name: currentNode,
+		weight: programsByName[currentNode].weight,
+		towerWeight: programsByName[currentNode].weight,
+	}
+	for _, childName := range programsByName[currentNode].children {
+		child := buildNode(programsByName, childName)
+		child.parent = p
+		p.children = append(p.children, child)
+		p.towerWeight += child.towerWeight
+	}
+	return p
+}
+
+var lineRexexp = regexp.MustCompile(`([a-z]+) \(([\d]+)\)(?: -> (.*))?`)
 
 func main() {
 	file, err := os.Open("day7/input.txt")
@@ -51,13 +128,17 @@ func main() {
 		line := scanner.Text()
 		lineMatches := lineRexexp.FindStringSubmatch(line)
 		name := lineMatches[1]
+		weight, _ := strconv.Atoi(lineMatches[2])
 		var children []string
-		if lineMatches[2] != "" {
-			children = strings.Split(lineMatches[2],", ")
+		if lineMatches[3] != "" {
+			children = strings.Split(lineMatches[3], ", ")
 		}
-		input = append(input, programSpec{name:name, children:children})
+		input = append(input, programSpec{name: name, weight: weight, children: children})
 	}
 
 	root := FindRoot(input)
-	fmt.Printf("Part 1: %s", root)
+	fmt.Printf("Part 1: %s\n", root)
+
+	node, targetWeight := FindProgramToAdjust(input)
+	fmt.Printf("Part 2: Adjust %s to weight %d\n", node, targetWeight)
 }
--- day7/day7_test.go
+++ day7/day7_test.go
@@ -17,23 +17,54 @@ func TestFindRoot(t *testing.T) {
 	//gyxo (61)
 	//cntj (57)
 	input := []programSpec{
-		{"pbga", nil},
-		{"xhth", nil},
-		{"ebii", nil},
-		{"havc", nil},
-		{"ktlj", nil},
-		{"fwft", []string{"ktlj", "cntj", "xhth"}},
-		{"qoyq", nil},
-		{"padx", []string{"pbga", "havc", "qoyq"}},
-		{"tknk", []string{"ugml", "padx", "fwft"}},
-		{"jptl", nil},
-		{"tknk", []string{"gyxo", "ebii", "jptl"}},
-		{"gyxo", nil},
-		{"cntj", nil},
+		{"pbga", 66,nil},
+		{"xhth", 57,nil},
+		{"ebii", 61,nil},
+		{"havc", 66,nil},
+		{"ktlj", 57,nil},
+		{"fwft", 72,[]string{"ktlj", "cntj", "xhth"}},
+		{"qoyq", 66,nil},
+		{"padx", 45,[]string{"pbga", "havc", "qoyq"}},
+		{"tknk", 41,[]string{"ugml", "padx", "fwft"}},
+		{"jptl", 61,nil},
+		{"ugml", 41,[]string{"gyxo", "ebii", "jptl"}},
+		{"gyxo", 61,nil},
+		{"cntj", 57,nil},
 	}
 
 	root := FindRoot(input)
 	if root != "tknk" {
 		t.Errorf("Expected root of tknk, got %s", root)
+	}
+}
+
+func TestFindProgramToAdjust(t *testing.T) {
+	input := []programSpec{
+		{"pbga", 66,nil},
+		{"xhth", 57,nil},
+		{"ebii", 61,nil},
+		{"havc", 66,nil},
+		{"ktlj", 57,nil},
+		{"fwft", 72,[]string{"ktlj", "cntj", "xhth"}},
+		{"qoyq", 66,nil},
+		{"padx", 45,[]string{"pbga", "havc", "qoyq"}},
+		{"tknk", 41,[]string{"ugml", "padx", "fwft"}},
+		{"jptl", 61,nil},
+		{"ugml", 41,[]string{"gyxo", "ebii", "jptl"}},
+		{"gyxo", 61,nil},
+		{"cntj", 57,nil},
+	}
+
+	//As you can see, tknk's disc is unbalanced: ugml's stack is heavier than the other two.
+	// Even though the nodes above ugml are balanced, ugml itself is too heavy:
+	// it needs to be 8 units lighter for its stack to weigh 243 and keep the towers balanced.
+	// If this change were made, its weight would be 60.
+
+	node, targetWeight := FindProgramToAdjust(input)
+	if node != "ugml" {
+		t.Errorf("Wrong target node, expected %s got %s", "ugml", node)
+	}
+	if targetWeight != 60 {
+		t.Errorf("Wrong target weight, expected %d got %d", 60, targetWeight)
 	}
 }
