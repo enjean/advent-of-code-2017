f86cd53437004fe6a0fa50e380e22e441609958f
Eric Jean <eric.jean@monsanto.com>
2019-06-18 13:57:21 -0600

    Day 10 Part 2

--- day10/day10.go
+++ day10/day10.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/enjean/advent-of-code-2017/adventutil"
 	"strconv"
@@ -24,28 +25,72 @@ func (s StringOfMarks) TieKnot(position, length int) {
 	}
 }
 
-func (s StringOfMarks) TieKnots(lengths []int) {
+func (s StringOfMarks) TieKnots(lengths []int, currentPosition, skipSize int) (int, int) {
 	lengthOfString := len(s)
-	currentPosition := 0
-	skipSize := 0
 	for _, length := range lengths {
 		s.TieKnot(currentPosition, length)
 		currentPosition = (currentPosition + length + skipSize) % lengthOfString
 		skipSize++
 		//fmt.Printf("%v %d %d\n", s, currentPosition, skipSize)
 	}
+	return currentPosition, skipSize
+}
+
+func ToLengths(input string) []int {
+	var lengths []int
+	for _, r := range input {
+		lengths = append(lengths, int(r))
+	}
+	lengths = append(lengths, 17, 31, 73, 47, 23)
+	return lengths
+}
+
+type SparseHash [256]int
+
+func CreateSparseHash(input string) SparseHash {
+	s := New(256)
+	lengths := ToLengths(input)
+	current, skip := 0, 0
+	for round := 0; round < 64; round++ {
+		current, skip = s.TieKnots(lengths, current, skip)
+	}
+
+	var sparseHash SparseHash
+	copy(sparseHash[:], s)
+	return sparseHash
+}
+
+func (sh SparseHash) ToDenseHash() string {
+	var denseHash [16]int
+	for i := 0; i < 16; i++ {
+		nums := sh[i*16 : (i+1)*16]
+		denseHash[i] = nums[0]
+		for j := 1; j < 16; j++ {
+			denseHash[i] ^= nums[j]
+		}
+	}
+
+	var b bytes.Buffer
+	for _, val := range denseHash {
+		b.WriteString(fmt.Sprintf("%02x", val))
+	}
+	return b.String()
 }
 
 func main() {
 	lines := adventutil.Parse(10)
 	var lengths []int
-	for _, lengthString := range strings.Split(lines[0],",") {
+	for _, lengthString := range strings.Split(lines[0], ",") {
 		val, _ := strconv.Atoi(lengthString)
 		lengths = append(lengths, val)
 	}
 
 	s := New(256)
-	s.TieKnots(lengths)
+	s.TieKnots(lengths, 0, 0)
 
-	fmt.Printf("Part 1: %d", s[0] * s[1])
+	fmt.Printf("Part 1: %d\n", s[0]*s[1])
+
+	sparseHash := CreateSparseHash(lines[0])
+	knotHash := sparseHash.ToDenseHash()
+	fmt.Printf("Part 2: %s\n", knotHash)
 }
--- day10/day10_test.go
+++ day10/day10_test.go
@@ -6,15 +6,15 @@ import (
 )
 
 func TestStringOfMarks_TieKnot(t *testing.T) {
-	tests := []struct{
-		before StringOfMarks
+	tests := []struct {
+		before           StringOfMarks
 		position, length int
-		after StringOfMarks
+		after            StringOfMarks
 	}{
-		{StringOfMarks{0,1,2,3,4},0, 3, StringOfMarks{2,1,0,3,4}},
-		{StringOfMarks{2,1,0,3,4},3, 4, StringOfMarks{4,3,0,1,2}},
-		{StringOfMarks{4,3,0,1,2},3, 1, StringOfMarks{4,3,0,1,2}},
-		{StringOfMarks{4,3,0,1,2},1, 5, StringOfMarks{3,4,2,1,0}},
+		{StringOfMarks{0, 1, 2, 3, 4}, 0, 3, StringOfMarks{2, 1, 0, 3, 4}},
+		{StringOfMarks{2, 1, 0, 3, 4}, 3, 4, StringOfMarks{4, 3, 0, 1, 2}},
+		{StringOfMarks{4, 3, 0, 1, 2}, 3, 1, StringOfMarks{4, 3, 0, 1, 2}},
+		{StringOfMarks{4, 3, 0, 1, 2}, 1, 5, StringOfMarks{3, 4, 2, 1, 0}},
 	}
 	for _, test := range tests {
 		b := make(StringOfMarks, len(test.before))
@@ -27,9 +27,40 @@ func TestStringOfMarks_TieKnot(t *testing.T) {
 }
 
 func TestStringOfMarks_TieKnots(t *testing.T) {
-	s := StringOfMarks{0,1,2,3,4}
-	s.TieKnots([]int{3,4,1,5})
-	if !reflect.DeepEqual(s, StringOfMarks{3,4,2,1,0}) {
+	s := StringOfMarks{0, 1, 2, 3, 4}
+	s.TieKnots([]int{3, 4, 1, 5}, 0, 0)
+	if !reflect.DeepEqual(s, StringOfMarks{3, 4, 2, 1, 0}) {
 		t.Errorf("TieKnots expected [3,4,2,1,0] got %v", s)
+	}
+}
+
+func TestToLengths(t *testing.T) {
+	result := ToLengths("1,2,3")
+	expected := []int{49, 44, 50, 44, 51, 17, 31, 73, 47, 23}
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Lengths incorrect, got %v", result)
+	}
+}
+
+func TestKnotHash(t *testing.T) {
+	//The empty string becomes a2582a3a0e66e6e86e3812dcb672a272.
+	//	AoC 2017 becomes 33efeb34ea91902bb2f59c9920caa6cd.
+	//1,2,3 becomes 3efbe78a8d82f29979031a4aa0b16a9d.
+	//1,2,4 becomes 63960835bcdc130f0b66d7ff4f6a5a8e.
+	tests := []struct {
+		input, expected string
+	}{
+		{"", "a2582a3a0e66e6e86e3812dcb672a272"},
+		{"AoC 2017", "33efeb34ea91902bb2f59c9920caa6cd"},
+		{"1,2,3", "3efbe78a8d82f29979031a4aa0b16a9d"},
+		{"1,2,4", "63960835bcdc130f0b66d7ff4f6a5a8e"},
+	}
+
+	for _, test := range tests {
+		sparseHash := CreateSparseHash(test.input)
+		result := sparseHash.ToDenseHash()
+		if result != test.expected {
+			t.Errorf("KnotHash(%s) expected %s got %s", test.input, test.expected, result)
+		}
 	}
 }
